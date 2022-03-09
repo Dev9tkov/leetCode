5c1e7e12cf0092550215fe22f48c7dbdbfb5ab0b
Ilya Devyatkov <idev9tkov@gmail.com>
2022-03-09 14:02:11 +0500

    contains duplicate

---
+++ src/main/java/arrays/containsDuplicate/Solution.java
@@ -0,0 +1,23 @@
+package arrays.containsDuplicate;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @see <a href="https://leetcode.com/problems/contains-duplicate/"></a>
+ * <p>
+ * Use 2 pointer
+ */
+public class Solution {
+    public boolean containsDuplicate(int[] nums) {
+
+        Set<Integer> set = new HashSet<>();
+        boolean result = false;
+        for (int i = 0; i < nums.length; i++) {
+            if (!set.add(nums[i])) {
+                return true;
+            }
+        }
+        return result;
+    }
+}
